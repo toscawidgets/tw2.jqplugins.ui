98caac822561926c850276fecc5b2a7f079acfe1
Ralph Bean <ralph.bean@gmail.com>
2010-09-15 11:38:01 -0400

    initial fork from tw2.jquery.core

---
+++ bin/develop-tw2-destroy-and-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+venv=virtualenv-tw2.jquery.ui
+$(
+    rm -rf $venv
+) || echo "Did not destroy $venv"
+
+virtualenv $venv --no-site-packages
+
+source $venv/bin/activate
+
+hg clone http://bitbucket.org/paj/tw2core || echo "tw2core exists."
+hg clone http://bitbucket.org/paj/tw2devtools || echo "tw2devtools exists."
+hg clone http://bitbucket.org/paj/tw2forms || echo "tw2devtools exists."
+git clone git://github.com/ralphbean/tw2.jquery.core.git || echo "tw2.jquery.core exists."
+
+pip install genshi
+pip install formencode
+
+cd tw2core ;  python setup.py develop ; cd -
+cd tw2forms ; python setup.py develop ; cd -
+cd tw2devtools ; python setup.py develop ; cd -
+cd tw2.jquery.core ; python setup.py develop ; cd -
+
