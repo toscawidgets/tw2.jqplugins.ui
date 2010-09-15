3b1b1286b612e8d6e44f16b2eaab2e8955dc5904
Ralph Bean <ralph.bean@gmail.com>
2010-09-15 12:20:36 -0400

    pruning forked stuff

--- tw2/jquery/core/base.py
+++ tw2/jquery/core/base.py
@@ -1,33 +1,10 @@
 from tw2.core.params import Param
-from tw2.core import CSSLink, DirLink, JSLink, js_function
-from version import JSLinkMixin
+from tw2.core import CSSLink, JSLink
+from tw2.core.jquery.version import JSLinkMixin
 
 import defaults
 
-class jQueryLinkMixin(JSLinkMixin):
-    name = 'jquery'
-    dirname = defaults._dirname_
-    basename = defaults._basename_
-    version = defaults._version_
-    url_base = defaults._url_base_
-
-    modname = 'tw2.jquery.core'
-
-class jQueryJSLink(JSLink, jQueryLinkMixin):
-    pass
-class jQueryCSSLink(jQueryLinkMixin, CSSLink):
-    extension = 'css'
-
-class jQueryPluginLinkMixin(jQueryLinkMixin):
-    dirname =   Param('(string) Specify the directory path for the given file, relative to the "static" folder some substitutions are allowed. Default: %s' % defaults._plugin_dirname_, default=defaults._plugin_dirname_)
-    basename =  Param('(string) Specify the basename for the given file. Default: %s' % defaults._plugin_basename_, default=defaults._plugin_basename_)
-    subdir = Param('(string) Specify the subdirectory that this link is found in.  Default: %s' % defaults._plugin_subdir_, default=defaults._plugin_subdir_)
-
-    @property
-    def substitutions(self):
-        subs = super(jQueryPluginLinkMixin, self).substitutions
-        subs.update(dict(subdir=self.subdir))
-        return subs
+from tw2.jquery.core.base import jQueryPluginLinkMixin
 
 class jQueryUIMixin(jQueryPluginLinkMixin):
     dirname = defaults._ui_dirname_
@@ -43,11 +20,3 @@ class jQueryUIThemeCSSLink(jQueryUIMixin, CSSLink):
         return 'css/%(name)s' % dict(name=self.name)
     extension = 'css'
 
-class jQueryPluginJSLink(JSLink, jQueryPluginLinkMixin):
-    subdir = 'js'
-
-class jQueryPluginCSSLink(jQueryPluginLinkMixin, CSSLink):
-    subdir = 'css'
-    extension = 'css'
-
-jQuery = js_function('jQuery')
