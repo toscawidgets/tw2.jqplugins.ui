4a51d7b8638330368e93b589d2b58c1d670d5ff8
Ralph Bean <ralph.bean@gmail.com>
2010-09-16 13:05:09 -0400

    got around the markup problem... yes

--- tw2/jquery/ui/widgets.py
+++ tw2/jquery/ui/widgets.py
@@ -80,14 +80,11 @@ class AccordionWidget(uibase.JQueryUIWidget):
 
     def prepare(self):
         super(AccordionWidget, self).prepare()
-
-        # TODO -- use strings or widgets or Markup or what?
-        # TODO -- if strings, how to unescape HTML?
-
-        # The following *does not work*.... what will? :/
-        # The html from samples.py shows up escaped on the client browser
-        from genshi.core import unescape as unes
-        self.items = [(unes(head), unes(cont)) for head, cont in self.items]
+        # The following is done to treat the items contents as 'html-literals'
+        class html(str):
+            def __html__(self):
+                return self.__str__()
+        self.items = [(html(h), html(c)) for h, c in self.items]
 
 
 class AutocompleteWidget(uibase.JQueryUIWidget):
@@ -812,5 +809,14 @@ class TabsWidget(uibase.JQueryUIWidget):
     """
     template = "tw2.jquery.ui.templates.tabs"
     jqmethod = "tabs"
+    
     items = twc.Param(
         'A list of (header (str), content (str)) tuples', default=[])
+    
+    def prepare(self):
+        super(TabsWidget, self).prepare()
+        # The following is done to treat the items contents as 'html-literals'
+        class html(str):
+            def __html__(self):
+                return self.__str__()
+        self.items = [(html(h), html(c)) for h, c in self.items]
