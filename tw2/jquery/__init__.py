4602200ed17b43c94c10a7903bd2643541ba5e7e
Ralph Bean <ralph.bean@gmail.com>
2010-10-19 14:00:17 -0400

    removed comment and fixed exception bug

--- tw2/jquery/__init__.py
+++ tw2/jquery/__init__.py
@@ -1,8 +1,5 @@
-# Upon installation of any of tw2.jquery.plugins.*, this
-#   file (originally in tw2.jquery) will be overwritten and
-#   therefore must be the same as in tw2.jquery proper.
 __import__('pkg_resources').declare_namespace(__name__)
 try:
     from widgets import *
-except ImportError:
+except ImportError, e:
     pass
