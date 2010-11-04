df2228fd2be5cdde8edb352473cded1506f185b2
Ralph Bean <ralph.bean@gmail.com>
2010-11-04 13:33:57 -0400

    using my devtools instead of paj

--- development-deps/develop-tw2-start.sh
+++ development-deps/develop-tw2-start.sh
@@ -5,11 +5,17 @@ venv=$devbase/virtualenv-tw2.jquery
 source $venv/bin/activate
 
 pushd $devbase
+
+pushd tw2.devtools
+python setup.py install
+popd
+
 rm -rf tw2jquery
 hg clone http://bitbucket.org/toscawidgets/tw2jquery
 pushd tw2jquery
 python setup.py install_lib install_egg_info
 popd
+
 popd
 
 python setup.py install_lib install_egg_info && paster tw2.browser
