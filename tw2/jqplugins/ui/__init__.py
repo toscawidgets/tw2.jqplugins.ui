21e1eae061755cf4e0616a7a0d2fc4969b23a0ea
Ralph Bean <ralph.bean@gmail.com>
2011-02-04 15:57:38 -0500

    forgot one leetle thing.

--- tw2/jqplugins/ui/__init__.py
+++ tw2/jqplugins/ui/__init__.py
@@ -1,6 +1,6 @@
 """ TW2 wrapper for the widgets from jquery UI (http://jqueryui.com)
 
-Get the source from http://github.com/ralphbean/tw2.jquery.plugins.ui
+Get the source from http://github.com/ralphbean/tw2.jqplugins.ui
 """
 
 from widgets import *
