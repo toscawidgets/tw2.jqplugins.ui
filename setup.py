b5cbda27df41d837cdf55059741ca63d38147176
Ralph Bean <ralph.bean@gmail.com>
2011-02-15 10:22:00 -0500

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ _extra_mako = ["Mako >= 0.1.1"]
 
 setup(
     name='tw2.jqplugins.ui',
-    version='2.0b9',
+    version='2.0b10',
     description='toscawidgets2 wrapper for jquery-ui',
     long_description=long_description,
     author='Ralph Bean',
