477f060adaacc3a6895b41f0c9d2051e68444508
Ralph Bean <ralph.bean@gmail.com>
2011-02-15 10:05:17 -0500

    Devised a much better method of specifying ui themes on a request/thread local basis

--- tw2/jqplugins/ui/base.py
+++ tw2/jqplugins/ui/base.py
@@ -34,19 +34,68 @@ jquery_ui_catcomplete_js = jQueryUIJSLink(version='custom',
 jquery_ui_js = jQueryUIJSLink(version=defaults._ui_version_)
 jquery_ui = jQueryJSLink(resources = [jquery_ui_css, jquery_ui_js])
 
+def set_ui_theme_name(ui_theme_name):
+    """ Set the jquery ui theme on a request/thread local basis 
+
+    Using Supported Themes
+    ----------------------
+
+    Supported themes live in ``tw2/jqplugins/ui/static/jquery/ui/version/css``
+
+    At present they include
+
+    - blitzer
+    - cupertino
+    - dark-hive
+    - eggplant
+    - excite-bike
+    - flick
+    - hot-sneaks
+    - humanity
+    - le-frog
+    - overcast
+    - pepper-grinder
+    - redmond
+    - smoothness
+    - south-street
+    - start
+    - sunny
+    - ui-darkness
+    - ui-lightness
+    - vader
+
+    Installing Your Own Theme
+    -------------------------
+
+    You could in theory install your own theme called, say, ``my-theme``.
+    The best way of doing this would be to build your theme with the jquery
+    ui `ThemeRoller <http://jqueryui.com/themeroller/>`_ and install it inside
+    your project.  Create a symlink pointing from
+    ``tw2/jqplugins/ui/static/jquery/ui/version/css/my-theme`` to the directory
+    where the theme is really held.  Then, at the beginning of each request
+    to your project, make the call ``set_ui_theme_name('my-theme')``
+
+    """
+    twc.core.request_local()['jquery_ui_theme_name'] = ui_theme_name
+
+def get_ui_theme_name():
+    """ Get the request/thread local jquery ui theme name """
+    if 'jquery_ui_theme_name' not in twc.core.request_local():
+        base = defaults._ui_theme_name_
+        twc.core.request_local()['jquery_ui_theme_name'] = base
+    return twc.core.request_local()['jquery_ui_theme_name']
+
 ### Widgets
 class JQueryUIWidget(twc.Widget):
     """ Base JQueryUIWidget """
     _hide_docs = False
-    resources = [ jquery_js, jquery_ui_js ]
+    resources = [ jquery_js, jquery_ui_js, jquery_ui_css ]
 
     jqmethod = twc.Variable("(str) Name of this widget's jQuery init method")
     selector = twc.Variable("(str) Escaped id.  jQuery selector.")
 
     options = twc.Param(
         '(dict) A dict of options to pass to the widget', default={})
-    ui_theme_name = twc.Param(
-        '(str) Name of the ui-theme to use.', default='smoothness')
 
     # TODO -- add all the events http://api.jquery.com/category/events/
     # TODO -- try to automatically generate IDs if not specified
@@ -54,7 +103,7 @@ class JQueryUIWidget(twc.Widget):
         '(str) javascript callback for generic click event', default=None)
     
     def prepare(self):
-        self.resources.append(jquery_ui_css(name=self.ui_theme_name))
+        self.resources.append(jquery_ui_css(name=get_ui_theme_name()))
         self.options = encoder.encode(self.options)
         super(JQueryUIWidget, self).prepare()
         if not hasattr(self, 'id') or 'id' not in self.attrs:
