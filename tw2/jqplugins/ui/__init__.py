f8f150b915381f58ecd2ba9f60961dc1cd94471a
Ralph Bean <ralph.bean@gmail.com>
2011-02-15 10:15:15 -0500

    Updated the package-level docstring

--- tw2/jqplugins/ui/__init__.py
+++ tw2/jqplugins/ui/__init__.py
@@ -1,5 +1,8 @@
 """ TW2 wrapper for the widgets from jquery UI (http://jqueryui.com)
 
+You can set the jquery-ui theme with
+``tw2.jqplugins.ui.base.set_ui_theme_name``.  The docstring of that function lists supported themes.
+
 Get the source from http://github.com/ralphbean/tw2.jqplugins.ui
 """
 
