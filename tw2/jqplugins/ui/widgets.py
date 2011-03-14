4eaa1f958f5456af85f088ef3ef88a207bd5b307
Ralph Bean <ralph.bean@gmail.com>
2011-03-13 23:29:09 -0400

    removed spurious import

--- tw2/jqplugins/ui/widgets.py
+++ tw2/jqplugins/ui/widgets.py
@@ -2,7 +2,6 @@
 # tw2-proper imports
 import tw2.core as twc
 import tw2.forms as twf
-from tw2.core.resources import encoder
 
 # imports from this package
 from tw2.jqplugins.ui import base as uibase
