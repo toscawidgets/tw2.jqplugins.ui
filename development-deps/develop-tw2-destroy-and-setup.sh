ee3e49c57eb231ace173cab61c1dec78235446f2
Ralph Bean <ralph.bean@gmail.com>
2011-05-16 13:14:26 -0400

    Smarter hg pulling.

--- development-deps/develop-tw2-destroy-and-setup.sh
+++ development-deps/develop-tw2-destroy-and-setup.sh
@@ -17,15 +17,15 @@ pip install mako
 pip install formencode
 
 hg clone http://bitbucket.org/paj/tw2core || \
-        (pushd tw2core && hg pull && popd)
+        (pushd tw2core && hg pull && hg update && popd)
 hg clone http://bitbucket.org/ralphbean/tw2.devtools || \
-        (pushd tw2.devtools && hg pull && popd)
+        (pushd tw2.devtools && hg pull && hg update && popd)
 hg clone http://bitbucket.org/paj/tw2forms || \
-        (pushd tw2forms && hg pull && popd)
+        (pushd tw2forms && hg pull && hg update && popd)
 hg clone http://bitbucket.org/ralphbean/tw2.jquery || \
-        (pushd tw2.jquery && hg pull && popd)
+        (pushd tw2.jquery && hg pull && hg update && popd)
 #hg clone https://ralphbean@bitbucket.org/toscawidgets/tw2jquery || \
-#        (pushd tw2jquery && hg pull && popd)
+#        (pushd tw2jquery && hg pull && hg update && popd)
 
 pushd tw2core ;  python setup.py install ; popd
 pushd tw2forms ; python setup.py install ; popd
