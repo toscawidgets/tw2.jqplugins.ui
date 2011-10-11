e15084b59e704a8162ff0d19da534dbd1647baff
Ralph Bean <ralph.bean@gmail.com>
2011-10-11 15:45:20 -0400

    Upgraded to jquery 1.8.16

--- tw2/jqplugins/ui/defaults.py
+++ tw2/jqplugins/ui/defaults.py
@@ -2,4 +2,4 @@
 _ui_theme_name_     = 'smoothness'
 _ui_dirname_        = 'jquery/ui/%(version)s/%(subdir)s'
 _ui_basename_       = 'jquery_ui'
-_ui_version_        = '1.8.9'
+_ui_version_        = '1.8.16'
