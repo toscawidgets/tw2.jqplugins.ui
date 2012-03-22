b2580c5944ad0c0d00241f2b15f8aa0c7f58370b
Ralph Bean <rbean@redhat.com>
2012-03-21 20:16:12 -0400

    Fixed a problem where jquery_ui.inject() would inject jquery_js twice.

---
+++ tests/test_resources.py
@@ -0,0 +1,39 @@
+import tw2.core as twc
+import tw2.jqplugins.ui
+import testapi
+
+from nose.tools import eq_
+
+js = twc.JSLink(link='paj')
+css = twc.CSSLink(link='joe')
+csssrc = twc.CSSSource(src='.bob { font-weight: bold; }')
+jssrc = twc.JSSource(src='bob')
+
+html = "<html><head><title>a</title></head><body>hello</body></html>"
+
+
+def simple_app(environ, start_response):
+    req = wo.Request(environ)
+    ct = 'text/html' if req.path == '/' else 'test/plain'
+    resp = wo.Response(request=req, content_type="%s; charset=UTF8" % ct)
+    resp.body = html
+    return resp(environ, start_response)
+
+mw = twc.make_middleware(simple_app)
+
+
+def setup():
+    testapi.setup()
+
+
+def test_weird_case_for_zykes():
+    rl = testapi.request(1, mw)
+    tw2.jqplugins.ui.jquery_ui.inject()
+
+    rl = twc.core.request_local()
+    r = rl['resources']
+    import pprint
+    pprint.pprint(rl['resources'])
+    for r in rl['resources']:
+        print '-', r
+    eq_(len(rl['resources']), 3)
