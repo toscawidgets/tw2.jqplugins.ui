51fa7b4af0945a02ed4c1ea2d66ae74cc668f1a7
Nils Philippsen <nils@redhat.com>
2019-01-12 16:56:56 +0100

    Update to jquery-ui 1.12.1.

--- tw2/jqplugins/ui/defaults.py
+++ tw2/jqplugins/ui/defaults.py
@@ -1,5 +1,5 @@
 #jQuery.ui
-_ui_theme_name_     = 'smoothness'
+_ui_theme_name_     = 'base'
 _ui_dirname_        = 'jquery/ui/%(version)s/%(subdir)s'
 _ui_basename_       = 'jquery_ui'
-_ui_version_        = '1.8.17'
+_ui_version_        = '1.12.1'
